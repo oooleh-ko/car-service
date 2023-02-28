62154d693a6e6fce1c10801faa3f9cea0fd4a818
Oleh <>
2023-02-28 21:30:48 +0200

    Add few test cars

--- src/main/java/com/collegesoft/carservice/services/CarService.java
+++ src/main/java/com/collegesoft/carservice/services/CarService.java
@@ -2,17 +2,34 @@ package com.collegesoft.carservice.services;
 
 import com.collegesoft.carservice.model.Car;
 import com.collegesoft.carservice.repositories.CarRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor
 public class CarService {
-    private final CarRepository repo;
+    public CarService(CarRepository repo) {
+        this.repo = repo;
+    }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        Car car = new Car();
+        car.setProductionYear(2020);
+        this.repo.save(car);
+        car = new Car();
+        car.setProductionYear(2015);
+        this.repo.save(car);
+        car = new Car();
+        car.setProductionYear(2022);
+        this.repo.save(car);
+    }
+
+    private final CarRepository repo;
 
     public Optional<Car> findById(Long id) {
         return this.repo.findById(id);
