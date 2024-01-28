68e47f7e2ff47ea755f3877ce329134938dee524
Oleh <>
2024-01-28 23:16:28 +0200

    Make price service call from controller

--- src/main/java/com/collegesoft/carservice/controllers/PriceController.java
+++ src/main/java/com/collegesoft/carservice/controllers/PriceController.java
@@ -3,13 +3,20 @@ package com.collegesoft.carservice.controllers;
 import com.collegesoft.carservice.services.PriceService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/price")
+@RestController
+@RequestMapping(value = "/price")
 public class PriceController {
     PriceService priceService;
-    @GetMapping(value = "/calculate/{id}")
-    public int getPrice(@PathVariable Long carId) {
+
+    public PriceController(PriceService priceService) {
+        this.priceService = priceService;
+    }
+
+    @GetMapping(value = "/calculate/{carId}")
+    public int getPrice(@PathVariable int carId) {
         return this.priceService.getPriceForCarDailyRent(carId, null);
     }
 }
